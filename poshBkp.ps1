69b83edcc50d5aca08001dab428cb24e3c9030d4
berinjelaboy <67279242+berinjelaboy@users.noreply.github.com>
2020-07-01 16:55:16 -0300

    Add files via upload

---
+++ poshBkp.ps1
@@ -0,0 +1,11 @@
+﻿Get-Command -Module PSScheduledJob | Sort-Object Noun
+
+$diario = New-JobTrigger -Daily at 2pm
+$umavez = New-JobTrigger -Once -At (Get-Date).AddHours(1)
+$semanal = New-JobTrigger -Weekly -DaysOfWeek Monday -At 6pm
+
+Register-ScheduledJob -name Backup -Trigger $diario -ScriptBlock {
+    Copy-Item C:\Scripts\*.* C:\OneDrive\Scripts\ -Recurse -Force
+}
+
+Get-ScheduledJob Backuup | Unregister-ScheduledJob
